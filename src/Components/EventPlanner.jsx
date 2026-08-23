1c409743433e08a25ad9333db266523eb765618b
cyramaeopiz87-sys <cyramaeopiz87@example.com>
2026-08-24 07:39:16 +0800

    completed 9 Added description content and get started button

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -8,6 +8,13 @@ const EventPlanner = () => {
                 <h1>Welcome to Event Planner</h1>
             </header>
             <section className="description">
+                {/* Brief introduction or marketing message */}
+                <p>
+                    Plan and organize your events effortlessly with Event Planner. From
+                    birthdays to corporate meetings, we've got you covered.
+                </p>
+                {/* Primary call-to-action button */}
+                <button className="get-started-button">Get Started</button>
     
 </section>
 
