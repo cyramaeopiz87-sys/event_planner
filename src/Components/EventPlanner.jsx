99786e98e10a618c6087ad2981f5e30776b4ce31
cyramaeopiz87-sys <cyramaeopiz87@example.com>
2026-08-24 10:04:08 +0800

    Added multiple testimonials

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -61,11 +61,18 @@ const EventPlanner = () => {
                     <li>Reminders and notifications</li>
                 </ul>
             </section>
-            <section className="testimonials">
+           <section className="testimonials">
+                {/* Section heading */}
                 <h2>Testimonials</h2>
+                {/* Individual testimonial block */}
                 <div className="testimonial">
                     <p>"Event Planner made organizing my wedding a breeze. Highly recommended!"</p>
                     <p className="author">- Emily Johnson</p>
+                </div>
+                {/* Another testimonial block */}
+                <div className="testimonial">
+                    <p>"I use Event Planner for all my corporate events. It saves me so much time and effort!"</p>
+                    <p className="author">- John Smith</p>
                 </div>
             </section>
             <section className="contact">
