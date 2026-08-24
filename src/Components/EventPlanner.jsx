0876d513297f077940a5a1caa317ef3de4a0f281
cyramaeopiz87-sys <cyramaeopiz87@example.com>
2026-08-24 10:14:03 +0800

    Completed 22  Added contact section with form

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -76,7 +76,19 @@ const EventPlanner = () => {
                 </div>
             </section>
             <section className="contact">
-                
+                {/* Section heading */}
+                <h2>Contact Us</h2>
+                {/* Contact form */}
+                <form>
+                    {/* Name input field */}
+                    <input type="text" placeholder="Name" />
+                    {/* Email input field */}
+                    <input type="email" placeholder="Email" />
+                    {/* Message textarea */}
+                    <textarea placeholder="Message"></textarea>
+                    {/* Submit button */}
+                    <button className="submit-button">Send</button>
+                </form>
             </section>
         </div>
     );
